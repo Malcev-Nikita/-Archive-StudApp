da52cbd3b566153c99f66a9b1d9fe87af1f76b84
Никита <mnd1work@gmail.com>
2023-04-19 19:16:04 +0400

    commit 19.4.23 (1)

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import Login from './src/components/Login';
 import { store } from './src/store';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Login />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Вход" component={Login} options={{ headerStyle: {backgroundColor: '#fff'}, headerTitleAlign: 'center'}} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
 
   );
